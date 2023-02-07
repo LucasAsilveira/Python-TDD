5d684ff782fff257c3c2ac7c5061c3d8b60227fe
LucasAsilveira <lucasaspal@gmail.com>
2023-02-07 08:36:12 -0300

    new test include

--- codigo/tests/test_bytebank.py
+++ codigo/tests/test_bytebank.py
@@ -13,3 +13,12 @@ class TestClass:
 
         # Then-desfecho
         assert resultado == esperado
+
+    def test_quando_sobrenome_recebe_Lucas_Carvalho_deve_retornar_apenas_carvalho(self):
+        entrada = ' Lucas Carvalho ' #Given
+        esperado = 'Carvalho' 
+
+        lucas = Funcionario(entrada, '11/11/2000', 1111)
+        resultado = lucas.sobrenome() # When
+
+        assert resultado == esperado
